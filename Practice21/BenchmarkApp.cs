e8769fd84f4dbaf4479cdcb59fadc9e52e20d676
JustIlnaz <ilnazikwork@gmail.com>
2026-04-13 22:43:13 +0300

    Добавьте файлы проекта.

---
+++ Practice21/BenchmarkApp.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Linq;
+
+namespace BenchmarkApp
+{
+    [MemoryDiagnoser]
+    public class SearchTest
+    {
+        private int[] data;
+        private int target = -1;
+
+        public SearchTest()
+        {
+            data = new int[10000];
+            for (int i = 0; i < data.Length; i++)
+                data[i] = i;
+        }
+
+        // 1. Обычный цикл
+        [Benchmark]
+        public bool ForLoopSearch()
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == target)
+                    return true;
+            }
+            return false;
+        }
+
+        // 2. LINQ
+        [Benchmark]
+        public bool LinqSearch()
+        {
+            return data.Contains(target);
+        }
+
+        // 3. Встроенный метод
+        [Benchmark]
+        public bool ArraySearch()
+        {
+            return Array.IndexOf(data, target) >= 0;
+        }
+    }
+}
---
+++ Practice21/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkApp;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SearchTest>();
+        }
+    }
+}
